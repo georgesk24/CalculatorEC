9b13b1265ad69898c1ee5260f30418f6c96405fb
jorge luis <jorgepotes1994@gmail.com>
2019-03-30 12:34:14 -0500

    new actualización determinante

---
+++ src/controller/ControllerDeterminanteMatrices.java
@@ -0,0 +1,171 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import view.PanelDeterminante;
+
+/**
+ *
+ * @author SOFTWARE GALAXIA TV  
+ */
+public class ControllerDeterminanteMatrices extends Controller implements ActionListener, KeyListener{
+    
+    PanelDeterminante view;
+
+    
+    
+    /*agregamos los respectivos eventos a utilizar*/
+    public final void events(){
+        view.JcOrdenDeterminante.addActionListener(this);
+        view.btnCalcular.addActionListener(this);
+        view.btnLimpiar.addActionListener(this);
+    }
+            
+    
+    public ControllerDeterminanteMatrices(PanelDeterminante view) {
+        this.view = view;
+    
+        /*agregamos nuevos componentes a nuestra vista (JTextField, JSeparator, JLabel)*/
+        
+        addTextField(view.JmatrizA,  0, 0, true, view.panelAreaOperaciones, true);
+        addSeparator(view.separator, 0, view.JmatrizA.length, view.panelAreaOperaciones, view.JmatrizA[0].length+1);
+        addTextField(view.JmatrizB,  0, view.JmatrizA.length+1, true, view.panelAreaOperaciones, true );        
+               
+        addLabel(new JLabel("Matriz A = "), 0, 0, view.panelAreaOperaciones, view.JmatrizA.length);
+        addLabel(new JLabel("Matriz B = "), 0, view.JmatrizA.length+1, view.panelAreaOperaciones, view.JmatrizA.length);                
+     
+        addTextField(view.JmatrizC, 0, 0, true, view.panelAreaResultado, false);
+        addLabel(new JLabel("Matriz C= "), 0, 0, view.panelAreaResultado, view.JmatrizC.length);
+
+        addEvents(view.JmatrizA);
+        addEvents(view.JmatrizB);
+        events();
+        
+    }
+
+    public final void addEvents(JTextField [][] array){
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                array[i][j].addKeyListener(this);
+            }
+        }
+    }    
+    
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        
+        Object evt = ae.getSource();
+        
+        /*se accionara cuando se seleccione un item del JCombobox*/
+        if(evt.equals(view.JcOrdenDeterminante)){
+            
+            /*obtenemos los item seleccionados del JcDimensionFila y JcDimensionColumna los cuales
+            haran referencia a las filas y columnas que llevara el arreglo bidimensional de JTextField*/
+            
+            int longitud = view.JcOrdenDeterminante.getSelectedIndex()+2;
+            
+            /*inicializamos los arreglos bidimensionales con sus nuevas dimensiones (filas y columnas) */
+            view.JmatrizA = new JTextField[longitud][longitud];
+            view.JmatrizB = new JTextField[longitud][longitud];   
+            view.JmatrizC = new JTextField[longitud][longitud];               
+            
+            /*removemos los componentes que estan agregados actualmente en los respectivos JPanel
+            panelAreaOperaciones y panelAreaResultado */
+            view.panelAreaOperaciones.removeAll();
+            view.panelAreaResultado.removeAll();
+            view.panelAreaResultado.setVisible(false);
+            /*llamamos los metodos addTextField, addSeparator y addLabel para agregar los nuevos componentes
+            en los respectivos JPanel*/
+            
+            addTextField(view.JmatrizA, 0, 0, true, view.panelAreaOperaciones, true);
+            addSeparator(view.separator, 0, longitud, view.panelAreaOperaciones, view.JmatrizA[0].length+1);
+            addTextField(view.JmatrizB,  0, longitud+1, true, view.panelAreaOperaciones, true );  
+            addLabel(new JLabel("Matriz A = "), 0, 0, view.panelAreaOperaciones, view.JmatrizA.length);
+            addLabel(new JLabel("Matriz B = "), 0, longitud+1, view.panelAreaOperaciones, view.JmatrizA.length);            
+
+            addTextField(view.JmatrizC, 0, 0, true, view.panelAreaResultado, false);
+            addLabel(new JLabel("Matriz C = "), 0, 0, view.panelAreaResultado, view.JmatrizC.length);
+
+            /*actualizamos los respectivos JPanel haciendo uso del metodo updateUI de esta manera se efectuaran
+            los cambios en nuestra ventana */
+
+            addEvents(view.JmatrizA);
+            addEvents(view.JmatrizB);            
+            
+            view.panelAreaOperaciones.updateUI();   
+            view.panelAreaResultado.updateUI();
+    
+        }else if(evt.equals(view.btnCalcular)){
+            /*
+            String [][] Ma, Mb;
+            
+            Ma = convertirArreglo(view.JmatrizA);                        
+            Mb = convertirArreglo(view.JmatrizB);
+            
+            if(!model.isEmptyArray(Ma) && !model.isEmptyArray(Mb)){
+
+                if(model.validateData(Ma)==true && model.validateData(Mb)==true){
+
+                    String Mc [][] = model.sumar(Ma, Mb);
+                    for (int i = 0; i < view.JmatrizC.length; i++) {
+                        for (int j = 0; j < view.JmatrizC[0].length; j++) {
+                            view.JmatrizC[i][j].setText(Mc[i][j]);
+                        }
+                    }
+
+                    view.panelAreaResultado.setVisible(true);            
+
+                }else{
+                    JOptionPane.showMessageDialog(null, "Formato no permitido\nVerifique sus datos e intente nuevamente", "ERROR", JOptionPane.ERROR_MESSAGE);
+                }
+                
+            }else{
+                JOptionPane.showMessageDialog(null, "Debe llenar todos los campos", "Opción valida", JOptionPane.ERROR_MESSAGE);
+            }*/
+            
+        }else if(evt.equals(view.btnLimpiar)){
+            
+            int confirmar = JOptionPane.showConfirmDialog(null, "¿Esta seguro que desea limpiar los campos?");
+            
+            if(confirmar==JOptionPane.YES_OPTION){
+
+                limpiarCampos(view.JmatrizA);
+                limpiarCampos(view.JmatrizB);
+                limpiarCampos(view.JmatrizC);
+                view.panelAreaResultado.setVisible(false);
+
+            }
+            
+        }
+        
+        
+        
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+    }
+    
+    
+    
+    
+}
--- src/view/PanelDeterminante.java
+++ src/view/PanelDeterminante.java
@@ -1,21 +1,40 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 package view;
 
+import controller.ControllerDeterminanteMatrices;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+
 /**
  *
- * @author de
+ * @author SOFTWARE GALAXIA
  */
 public class PanelDeterminante extends javax.swing.JPanel {
 
-    /**
-     * Creates new form SumarMatrices
-     */
+    /*Creamos tres arreglos bidimensionales de tipo JTextField los cuales haran referencia
+    a las matrices que trabajaremos */    
+    public JTextField [][] JmatrizA, JmatrizB, JmatrizC; 
+    public JSeparator separator;
+    
+    ControllerDeterminanteMatrices controller;
+    
     public PanelDeterminante() {
+        
         initComponents();
+        
+        panelAreaResultado.setVisible(false);
+        
+        /*Asignamos dimensiones a los arreglos */
+        JmatrizA = new JTextField[2][2];
+        JmatrizB= new JTextField[2][2];        
+        JmatrizC = new JTextField[2][2]; 
+        
+        separator = new JSeparator();
+        
+        controller = new ControllerDeterminanteMatrices(this);
+        
     }
 
     /**
@@ -28,22 +47,111 @@ public class PanelDeterminante extends javax.swing.JPanel {
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
 
-        jLabel1 = new javax.swing.JLabel();
+        lbTitle = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        JcOrdenDeterminante = new javax.swing.JComboBox<>();
+        panelAreaOperaciones = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        btnCalcular = new javax.swing.JButton();
+        btnLimpiar = new javax.swing.JButton();
+        panelAreaResultado = new javax.swing.JPanel();
 
-        setBackground(new java.awt.Color(51, 255, 255));
+        setBackground(new java.awt.Color(255, 255, 255));
         setLayout(new java.awt.GridBagLayout());
 
-        jLabel1.setText("Determinante");
+        lbTitle.setFont(new java.awt.Font("Times New Roman", 3, 36)); // NOI18N
+        lbTitle.setText("Determinante De Una Matriz");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(130, 185, 156, 185);
-        add(jLabel1, gridBagConstraints);
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 20, 0);
+        add(lbTitle, gridBagConstraints);
+
+        jLabel2.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        jLabel2.setText("Orden: ");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(5, 20, 0, 10);
+        add(jLabel2, gridBagConstraints);
+
+        JcOrdenDeterminante.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "2x2", "3x3", "4x4", "5x5", "6x6", "7x7", "8x8" }));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 70;
+        gridBagConstraints.ipady = 10;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 0);
+        add(JcOrdenDeterminante, gridBagConstraints);
+
+        panelAreaOperaciones.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Datos De Entrada", javax.swing.border.TitledBorder.LEFT, javax.swing.border.TitledBorder.TOP));
+        panelAreaOperaciones.setOpaque(false);
+        panelAreaOperaciones.setLayout(new java.awt.GridBagLayout());
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.ipady = 20;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(20, 20, 20, 20);
+        add(panelAreaOperaciones, gridBagConstraints);
+
+        jPanel2.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED, new java.awt.Color(153, 153, 153), new java.awt.Color(204, 204, 204), new java.awt.Color(153, 153, 153), new java.awt.Color(153, 153, 153)));
+        jPanel2.setOpaque(false);
+        jPanel2.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, 10, 5));
+
+        btnCalcular.setBackground(new java.awt.Color(255, 83, 83));
+        btnCalcular.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        btnCalcular.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/calcular-suma.png"))); // NOI18N
+        btnCalcular.setText("Calcular");
+        btnCalcular.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        btnCalcular.setIconTextGap(14);
+        btnCalcular.setPreferredSize(new java.awt.Dimension(227, 67));
+        jPanel2.add(btnCalcular);
+
+        btnLimpiar.setBackground(new java.awt.Color(123, 167, 225));
+        btnLimpiar.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
+        btnLimpiar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/clean.png"))); // NOI18N
+        btnLimpiar.setText("Limpiar");
+        btnLimpiar.setHorizontalTextPosition(javax.swing.SwingConstants.LEFT);
+        btnLimpiar.setIconTextGap(14);
+        btnLimpiar.setPreferredSize(new java.awt.Dimension(227, 67));
+        jPanel2.add(btnLimpiar);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 10, 20);
+        add(jPanel2, gridBagConstraints);
+
+        panelAreaResultado.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Resultado", javax.swing.border.TitledBorder.LEFT, javax.swing.border.TitledBorder.TOP));
+        panelAreaResultado.setOpaque(false);
+        panelAreaResultado.setLayout(new java.awt.GridBagLayout());
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.insets = new java.awt.Insets(10, 20, 10, 20);
+        add(panelAreaResultado, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    public javax.swing.JComboBox<String> JcOrdenDeterminante;
+    public javax.swing.JButton btnCalcular;
+    public javax.swing.JButton btnLimpiar;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lbTitle;
+    public javax.swing.JPanel panelAreaOperaciones;
+    public javax.swing.JPanel panelAreaResultado;
     // End of variables declaration//GEN-END:variables
 }
